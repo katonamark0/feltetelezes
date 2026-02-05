a84cbf3628cee81214bab036eff274bdebb408c4
Katona Márk <katonamark0919@gmail.com>
2026-02-05 15:30:21 +0100

    Update feltetel.js

--- feltetel.js
+++ feltetel.js
@@ -42,11 +42,19 @@
     // console.log("A szám egész" + szam3)
 // }
 
-// 5. A program kérdezzen két számot, s utána írja ki a köztük lévő relációt. Például, ha a két
-// szám 3 és -6.12, akkor az eredmény: 3 > -6.12.
+//5. A program kérdezzen két számot, s utána írja ki a köztük lévő relációt. Például, ha a két
+//szám 3 és -6.12, akkor az eredmény: 3 > -6.12.
 // 
 // let szam4 = parseFloat(prompt("Kérem az egyik számot: "))
 // let szam5 = parseFloat(prompt("Kérem a másik számot: "))
+//
+//if(szam4 > szam5){
+//    console.log(szam4 + "A nagyobb");
+//}else if(szam5 > szam4){
+//    console.log(szam5 + "A nagyobb");
+//}else{
+//    console.log("A két szám egyenlő: " + szam4);
+//}
 
 
 // 6. Írj programot, ami egy életkor alapján eldönti, hogy gyerek (0-6 év), iskolás (7-18),
@@ -118,21 +126,72 @@
 // évszám maradék nélkül osztható 4-gyel, de nem osztható 100-zal, kivéve, ha az évszám
 // osztható 400-zal.
 
-let szokoEv = false;
-let evszam = parseInt(prompt("Kérek egy évet: "))
+//et szokoEv = false;
+//et evszam = parseInt(prompt("Kérek egy évet: "))
+//
+//f(evszam % 400 === 0){
+//   szokoEv = true
+//   
+//else if(evszam % 100 === 0){
+//   szokoEv = false
+//else if(evszam % 4 === 0){
+//   szokoEv = true
+//
+//
+//f (szokoEv){
+//   console.log("Ez az évszám  szökőév: " + evszam);
+//else{
+//   console.log("Ez az évszám nem szökőév: " + evszam);
+//   
+//
 
-if(evszam % 400 === 0){
-    szokoEv = true
-    
-}else if(evszam % 100 === 0){
-    szokoEv = false
-}else if(evszam % 4 === 0){
-    szokoEv = true
-}
+//14. Kérjük be egy hónap sorszámát, majd írjuk ki a nevét!
 
-if (szokoEv){
-    console.log("Ez az évszám  szökőév: " + evszam);
-}else{
-    console.log("Ez az évszám nem szökőév: " + evszam);
-    
-}
+//let honapSzam = parseInt(prompt("Kérem a hónap sorszámát (1-12): "));
+//
+//if(honapSzam === 1){
+//    console.log("Január");
+//}else if(honapSzam === 2){
+//    console.log("Február");
+//}else if(honapSzam === 3){
+//    console.log("Március");
+//}else if(honapSzam === 4){
+//    console.log("Április");
+//}else if(honapSzam === 5){
+//    console.log("Május");
+//}else if(honapSzam === 6){
+//    console.log("Június");
+//}else if(honapSzam === 7){
+//    console.log("Július");
+//}else if(honapSzam === 8){
+//    console.log("Augusztus");
+//}else if(honapSzam === 9){
+//    console.log("Szeptember");
+//}else if(honapSzam === 10){
+//    console.log("Október");
+//}else if(honapSzam === 11){
+//    console.log("November");
+//}else if(honapSzam === 12){
+//    console.log("December");
+//}else{
+//    console.log("Itt valami nem jó, próbáld újra!");
+//}
+
+//15. Kérjük be egy dolgozat pontszámát 1 és 100 között és írjuk ki az érdemjegyet! 0-40:
+//elégtelen, 41-55: elégséges, 56-70: közepes, 71-85: jó, 86-100: jeles.
+
+//let pontszam = parseInt(prompt("Kérem a dolgozat pontszámát (1-100): "));
+//
+//if(pontszam >= 0 && pontszam <= 40){
+//    console.log("Érdemjegy: elégtelen");
+//}else if(pontszam >= 41 && pontszam <= 55){
+//    console.log("Érdemjegy: elégséges");
+//}else if(pontszam >= 56 && pontszam <= 70){
+//    console.log("Érdemjegy: közepes");
+//}else if(pontszam >= 71 && pontszam <= 85){
+//    console.log("Érdemjegy: jó");
+//}else if(pontszam >= 86 && pontszam <= 100){
+//    console.log("Érdemjegy: jeles");
+//}else{
+//    console.log("Valamit félrenyomtál kicsi!");
+//}
